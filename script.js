3f8adb340e9e771732496e7379b1b40a0aa3e26f
unknown <MIW@A-F004358.salsa.univ-amu.fr>
2018-10-11 16:26:08 +0200

    Ajout de la carte

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let map, markers = [];
+(function () {
+    initMap();
+})();
+
 function getCityByPostalCode(postalCode) {
     let startTime = Date.now();
 
@@ -20,12 +25,18 @@ function getCityByPostalCode(postalCode) {
 }
 
 function writesData(response) {
-    let tabbleBody = document.getElementById('dataCityRow');
+    let tabbleBody = document.getElementById('dataCityRow'),
+        mapBounds = [];
     tabbleBody.innerHTML = '';
-
+    clearMarkers();
     for (let city of response.data.features) {
+        let lat = city.geometry.coordinates[1],
+            lgn = city.geometry.coordinates[0];
         tabbleBody.innerHTML += rowTemplate(city);
+        addMakerInMap(lat, lgn, city.properties.nom);
+        mapBounds.push([lat,lgn])
     }
+    map.fitBounds(mapBounds);
 }
 
 function rowTemplate(data) {
@@ -38,8 +49,8 @@ function rowTemplate(data) {
     <td>${pro.code}</td>
     <td>${pro.population}</td>
     <td>
-        ${geo.coordinates[0].toFixed(fixedNumber)},
-        ${geo.coordinates[1].toFixed(fixedNumber)}
+        ${geo.coordinates[1].toFixed(fixedNumber)},
+        ${geo.coordinates[0].toFixed(fixedNumber)}
     </td>
 </tr>`;
 }
@@ -50,4 +61,21 @@ function sumPopulation(cities) {
         population += city.properties.population;
     }
     return population;
+}
+
+function initMap() {
+    map = L.map('map').setView([44.562201, 6.079159], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
+function addMakerInMap(lat, lng, name) {
+    markers.push(
+        L.marker([lat, lng]).addTo(map).bindPopup(name)
+    );
+}
+function clearMarkers() {
+    for(let marker of markers) {
+        marker.remove();
+    }
 }
